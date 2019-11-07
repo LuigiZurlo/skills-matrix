fbc6ef036708e9da52c63f984a3280b1336cd7a9
Luigi Zurlo <luigizurlo@MacBook-Pro-di-Luigi.local>
2019-11-07 10:58:25 +0100

    refactoring competencyGroups apis

--- skills-matrix-be/src/api/competency_groups/competency_group.controller.ts
+++ skills-matrix-be/src/api/competency_groups/competency_group.controller.ts
@@ -1,20 +1,55 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 import {ApiResponse} from "../../common/api.response.model";
 import {ErrorHandler} from "../../common/Error";
-import { db, pgp } from "../../db/db";
+import {db, pgp} from "../../db/db";
 
 export default class CompetencyGroupController {
+
   public getCompetencyGroups = async (req: Request, res: Response, next: any): Promise<any> => {
+    try {
+
+      const competencyGroups = await db.any("SELECT * FROM competency_groups");
+
+      if (Object.keys(competencyGroups).length === 0) {
+        throw new ErrorHandler(404, "CompetencyGroups not found");
+      }
+
+      res.status(200).send(new ApiResponse(true, "CompetencyGroups found", competencyGroups, 200));
+      next();
+
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  public getCompetencyGroup = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const competencyGroups = await db.any("SELECT * FROM competency_groups WHERE id = $1",
         [req.params.competency_group_id]);
 
       if (Object.keys(competencyGroups).length === 0) {
-        throw new ErrorHandler(404, "Competency group not found");
+        throw new ErrorHandler(404, "CompetencyGroup not found");
       }
-      res.status(200).send(new ApiResponse(true, "Competency group found", competencyGroups, 200));
+
+      res.status(200).send(new ApiResponse(true, "CompetencyGroup found", competencyGroups, 200));
       next();
+
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  public createCompetencyGroups = async (req: Request, res: Response, next: any): Promise<any> => {
+    try {
+
+      const competencyGroups = await db.one("INSERT INTO competency_groups (position_id, name) " +
+        "VALUES ($1, $2) " +
+        "RETURNING *", [req.body.position_id, req.body.name]);
+
+      res.status(201).send(new ApiResponse(true, "CompetencyGroup successfully created", competencyGroups, 201));
+      next();
+
     } catch (err) {
       next(err);
     }
@@ -23,7 +58,7 @@ export default class CompetencyGroupController {
   public updateCompetencyGroups = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
       const competencyGroupColumnSet = new pgp.helpers.ColumnSet(
-        ["?position_id", "name"],
+        ["position_id", "name"],
         {table: "competency_groups"});
       const compValues = req.body;
       const compQuery = pgp.helpers.update(compValues, competencyGroupColumnSet) + " WHERE id = $1";
@@ -38,5 +73,34 @@ export default class CompetencyGroupController {
       next(err);
     }
   }
+
+  public deleteCompetencyGroup = async (req: Request, res: Response, next: any): Promise<any> => {
+    try {
+
+      const competencies = await db.any("DELETE FROM competency_groups " +
+        "WHERE id = $1 " +
+        "RETURNING *", [req.params.competency_group_id]);
+
+      res.status(200).send(new ApiResponse(true, "CompetencyGroup successfully deleted", competencies, 200));
+      next();
+
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  public deleteCompetencyGroups = async (req: Request, res: Response, next: any): Promise<any> => {
+    try {
+
+      const competencies = await db.any("DELETE FROM competencies RETURNING *", []);
+
+      res.status(200).send(new ApiResponse(true, "CompetencyGroups successfully deleted", competencies, 200));
+      next();
+
+    } catch (err) {
+      next(err);
+    }
+  }
+
 
 }
--- skills-matrix-be/src/api/competency_groups/competency_group.route.ts
+++ skills-matrix-be/src/api/competency_groups/competency_group.route.ts
@@ -5,7 +5,13 @@ const competencyGroups: Router = Router();
 
 const competencyGroupController = new CompetencyGroupController();
 
-competencyGroups.get("/:competency_group_id", competencyGroupController.getCompetencyGroups);
+competencyGroups.get("/", competencyGroupController.getCompetencyGroups);
+competencyGroups.post("/", competencyGroupController.createCompetencyGroups);
+competencyGroups.delete("/", competencyGroupController.deleteCompetencyGroups);
+
+competencyGroups.get("/:competency_group_id", competencyGroupController.getCompetencyGroup);
 competencyGroups.put("/:competency_group_id", competencyGroupController.updateCompetencyGroups);
+competencyGroups.delete("/:competency_group_id", competencyGroupController.deleteCompetencyGroup);
+
 
 export default competencyGroups;
