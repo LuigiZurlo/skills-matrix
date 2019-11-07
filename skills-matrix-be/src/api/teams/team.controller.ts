a64b8ce1c9fb027a93325f2f4e838da8a0e0a542
Luigi Zurlo <luigizurlo@MacBook-Pro-di-Luigi.local>
2019-11-07 16:48:57 +0100

    refactoring teams apis

--- skills-matrix-be/src/api/teams/team.controller.ts
+++ skills-matrix-be/src/api/teams/team.controller.ts
@@ -1,11 +1,11 @@
 import {Request, Response} from "express";
-import {db, pgp} from "../../db/db";
 import {ApiResponse} from "../../common/api.response.model";
 import {ErrorHandler} from "../../common/Error";
+import {db, pgp} from "../../db/db";
 
 export default class TeamController {
 
-  public getTeams = async (req: Request, res: Response): Promise<any> => {
+  public getTeams = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const validQueryParams = ["project_id"];
@@ -18,60 +18,36 @@ export default class TeamController {
       }
 
       if (Object.keys(teams).length === 0) {
-        return res.status(404).send({
-          data: teams,
-          message: "Teams not found",
-          success: false,
-        });
+        throw new ErrorHandler(404, "Teams not found");
       }
 
-      res.status(200).send({
-        data: teams,
-        data_length: teams.length,
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Teams found", teams, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err.toString(),
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public getTeam = async (req: Request, res: Response): Promise<any> => {
+  public getTeam = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
-      const teams = await db.one("SELECT * FROM project_teams where id = $1", [req.params.team_id]);
+      const teams = await db.any("SELECT * FROM project_teams where id = $1", [req.params.team_id]);
 
-      if (Object.keys(teams).length === 0) {
-        return res.status(404).send({
-          data: null,
-          message: "Team not found",
-          success: false,
-        });
+      if (teams.length === 0) {
+        throw new ErrorHandler(404, "Team not found");
+
       }
 
-      res.status(200).send({
-        data: teams,
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Team found", teams, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err.toString(),
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public createTeams = async (req: Request, res: Response): Promise<any> => {
+  public createTeams = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       // Create Teams
@@ -80,95 +56,64 @@ export default class TeamController {
       const teamsQuery = pgp.helpers.insert(teamsValues, teamsColumnSet) + " ON CONFLICT DO NOTHING RETURNING *";
       const teamsResult = await db.any(teamsQuery);
 
-      res.status(201).send({
-        data: teamsResult,
-        message: "Team(s) successfully created",
-        success: true,
-      });
+      res.status(201).send(new ApiResponse(true, "Team(s) successfully created", teamsResult, 201));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err.toString(),
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public deleteTeam = async (req: Request, res: Response): Promise<any> => {
+  public deleteTeam = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const rows = await db.any("DELETE FROM project_teams WHERE id = $1 RETURNING *", [req.params.team_id]);
 
-      res.status(200).send({
-        data: rows,
-        message: "Team successfully deleted",
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Team successfully deleted", rows, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err.toString(),
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public getTeamResources = async (req: Request, res: Response): Promise<any> => {
+  public getTeamResources = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const teamResources = await db.any("" +
         "SELECT * FROM resources r WHERE r.id " +
         "IN (SELECT tm.resource_id FROM team_memberships tm WHERE tm.team_id = $1)", [req.params.team_id]);
 
-      res.status(200).send({
-        data: teamResources,
-        data_length: teamResources.length,
-        success: true,
-      });
+      if (teamResources.length === 0) {
+        throw new ErrorHandler(404, "TeamResources not found");
+      }
+
+      res.status(200).send(new ApiResponse(true, "TeamResources found", teamResources, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err.toString(),
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public addTeamResources = async (req: Request, res: Response): Promise<any> => {
+  public addTeamResources = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const teamsResourcesColumnSet = new pgp.helpers.ColumnSet(
         ["team_id", "resource_id"], {table: "team_memberships"});
-      const teamResourcesValues = [];
-      for (const resource of req.body) {
+      const teamResourcesValues = req.body;
+      /*for (const resource of req.body) {
         teamResourcesValues.push({team_id: req.params.team_id, resource_id: resource.id});
-      }
+      }*/
+
       const teamResourcesQuery = pgp.helpers.insert(teamResourcesValues, teamsResourcesColumnSet) + " ON CONFLICT DO NOTHING RETURNING *";
       const teamResourcesResult = await db.any(teamResourcesQuery);
 
-      res.status(201).send({
-        data: teamResourcesResult,
-        message: "Resources successfully added to team",
-        success: true,
-      });
+      res.status(201).send(new ApiResponse(true, "Resources successfully added to team", teamResourcesResult, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err.toString(),
-        success: false,
-      });
-
+      next(err);
     }
   }
 
@@ -190,6 +135,5 @@ export default class TeamController {
       next(err);
     }
   }
-
 
 }
