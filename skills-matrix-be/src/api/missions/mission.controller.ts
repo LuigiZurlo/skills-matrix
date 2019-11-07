74067001013a0175d3998add1c1eccb6b91c44fe
Luigi Zurlo <luigizurlo@MacBook-Pro-di-Luigi.local>
2019-11-07 11:17:20 +0100

    refactoring missions apis

--- skills-matrix-be/src/api/missions/mission.controller.ts
+++ skills-matrix-be/src/api/missions/mission.controller.ts
@@ -1,139 +1,87 @@
-import { Request, Response } from "express";
-import {db, pgp} from "../../db/db";
+import {Request, Response} from "express";
 import {ApiResponse} from "../../common/api.response.model";
 import {ErrorHandler} from "../../common/Error";
+import {db, pgp} from "../../db/db";
 
 export default class MissionController {
 
-  public getMissions = async (req: Request, res: Response): Promise<any> => {
+  public getMissions = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const missions = await db.any("SELECT * FROM missions", []);
 
       if (Object.keys(missions).length === 0) {
-        return res.status(404).send({
-          data: missions,
-          message: "Missions not found",
-          success: false,
-        });
+        throw new ErrorHandler(404, "Missions not found");
       }
 
-      res.status(200).send({
-        data: missions,
-        data_length: missions.length,
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Missions found", missions, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public getMission = async (req: Request, res: Response): Promise<any> => {
+  public getMission = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const missions = await db.any("SELECT * FROM missions where id = $1", [req.params.mission_id]);
 
       if (Object.keys(missions).length === 0) {
-        return res.status(404).send({
-          data: missions,
-          message: "Mission not found",
-          success: false,
-        });
+        throw new ErrorHandler(404, "Mission not found");
       }
 
-      res.status(200).send({
-        data: missions,
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Mission found", missions, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public createMissions = async (req: Request, res: Response): Promise<any> => {
+  public createMissions = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
-      const missions = await db.one("INSERT INTO missions (resource_id, project_id, position_id, start_date, end_date) VALUES ($1, $2, $3, $4) RETURNING *", [req.body.resource_id, req.body.project_id, req.body.position_id, req.body.start_date, req.body.end_date]);
+      const missions = await db.one("INSERT INTO missions (resource_id, project_id, position_id, start_date, end_date) VALUES ($1, $2, $3, $4, $5) RETURNING *", [req.body.resource_id, req.body.project_id, req.body.position_id, req.body.start_date, req.body.end_date]);
 
-      res.status(201).send({
-        data: missions,
-        message: "Mission successfully created",
-        success: true,
-      });
+      res.status(201).send(new ApiResponse(true, "Mission successfully created", missions, 201));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public deleteMission = async (req: Request, res: Response): Promise<any> => {
+  public deleteMission = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const missions = await db.any("DELETE FROM missions WHERE id = $1 RETURNING *", [req.params.mission_id]);
 
-      res.status(200).send({
-        data: missions,
-        message: "Mission successfully deleted",
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Mission successfully deleted", missions, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public deleteMissions = async (req: Request, res: Response): Promise<any> => {
+  public deleteMissions = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const missions = await db.any("DELETE FROM missions RETURNING *", []);
 
-      res.status(200).send({
-        data: missions,
-        message: "Missions successfully deleted",
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Missions successfully deleted", missions, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
   public updateMissions = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
       const missionsColumnSet = new pgp.helpers.ColumnSet(
-        ["?resource_id", "?project_id", "?position_id", "?start_date", "?end_data", "is_active"],
+        ["resource_id", "project_id", "position_id", "start_date", "end_date", "is_active"],
         {table: "missions"});
       const missionsValues = req.body;
       const missionsQuery = pgp.helpers.update(missionsValues, missionsColumnSet) + " WHERE id = $1";
@@ -144,6 +92,7 @@ export default class MissionController {
         throw new ErrorHandler(400, "Bad request");
       }
       next();
+
     } catch (err) {
       next(err);
     }
