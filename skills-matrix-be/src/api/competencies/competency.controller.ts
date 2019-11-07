0e1ec8337538285466dd834e1cdebafc3e8d38e1
Luigi Zurlo <luigizurlo@MacBook-Pro-di-Luigi.local>
2019-11-07 10:42:16 +0100

    refactoring competencies apis

--- skills-matrix-be/src/api/competencies/competency.controller.ts
+++ skills-matrix-be/src/api/competencies/competency.controller.ts
@@ -1,143 +1,91 @@
 import {Request, Response} from "express";
-import {db, pgp} from "../../db/db";
 import {ApiResponse} from "../../common/api.response.model";
 import {ErrorHandler} from "../../common/Error";
+import {db, pgp} from "../../db/db";
 
 export default class CompetencyController {
 
-  public getCompetencies = async (req: Request, res: Response): Promise<any> => {
+  public getCompetencies = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const competencies = await db.any("SELECT * FROM competencies", []);
 
       if (Object.keys(competencies).length === 0) {
-        return res.status(404).send({
-          data: competencies,
-          message: "Competencies not found",
-          success: false,
-        });
+        throw new ErrorHandler(404, "Competencies not found");
       }
 
-      res.status(200).send({
-        data: competencies,
-        data_length: competencies.length,
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Competencies found", competencies, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public getCompetency = async (req: Request, res: Response): Promise<any> => {
+  public getCompetency = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const competencies = await db.any("SELECT * FROM competencies where id = $1", [req.params.competency_id]);
 
       if (Object.keys(competencies).length === 0) {
-        return res.status(404).send({
-          data: competencies,
-          message: "Competency not found",
-          success: false,
-        });
+        throw new ErrorHandler(404, "Competency not found");
       }
 
-      res.status(200).send({
-        data: competencies,
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Competency found", competencies, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public createCompetencies = async (req: Request, res: Response): Promise<any> => {
+  public createCompetencies = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const competencies = await db.one("INSERT INTO competencies (skill_id, level) " +
         "VALUES ($1, $2) " +
         "RETURNING *", [req.body.skill_id, req.body.level]);
 
-      res.status(201).send({
-        data: competencies,
-        message: "Competency successfully created",
-        success: true,
-      });
+      res.status(201).send(new ApiResponse(true, "Competency successfully created", competencies, 201));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public deleteCompetency = async (req: Request, res: Response): Promise<any> => {
+  public deleteCompetency = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const competencies = await db.any("DELETE FROM competencies " +
         "WHERE id = $1 " +
         "RETURNING *", [req.params.competency_id]);
 
-      res.status(200).send({
-        data: competencies,
-        message: "Competency successfully deleted",
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Competency successfully deleted", competencies, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
-  public deleteCompetencies = async (req: Request, res: Response): Promise<any> => {
+  public deleteCompetencies = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
 
       const competencies = await db.any("DELETE FROM competencies RETURNING *", []);
 
-      res.status(200).send({
-        data: competencies,
-        message: "Competencies successfully deleted",
-        success: true,
-      });
+      res.status(200).send(new ApiResponse(true, "Competencies successfully deleted", competencies, 200));
+      next();
 
     } catch (err) {
-
-      res.status(500).send({
-        data: null,
-        message: err,
-        success: false,
-      });
-
+      next(err);
     }
   }
 
   public updateCompetency = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
       const positionsColumnSet = new pgp.helpers.ColumnSet(
-        ["?skill_id", "level"],
+        ["skill_id", "level"],
         {table: "competencies"});
       const competenciesValues = req.body;
       const competenciesQuery = pgp.helpers.update(competenciesValues, positionsColumnSet) + " WHERE id = $1 RETURNING *";
--- skills-matrix-be/src/config/winston.js
+++ skills-matrix-be/src/config/winston.js
@@ -20,7 +20,6 @@ const options = {
       label({label: 'Logger:'}),
       timestamp(),
       myFormat,
-      winston.format.json(),
     ),
   },
   console: {
