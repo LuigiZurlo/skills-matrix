2986f279ac261bf8bb59a75e50fc4afe86e14b6e
Luigi Zurlo <luigizurlo@MacBook-Pro-di-Luigi.local>
2019-11-07 15:48:18 +0100

    refactoring resources apis

--- skills-matrix-be/src/api/resources/resource.route.ts
+++ skills-matrix-be/src/api/resources/resource.route.ts
@@ -10,8 +10,13 @@ resource.post("/", resourceController.createResources);
 
 resource.get("/:resource_id", resourceController.getResource);
 resource.put("/:resource_id", resourceController.updateResource);
+resource.delete("/:resource_id", resourceController.deleteResource);
 
 resource.get("/:resource_id/competencies", resourceController.getResourceCompetencies);
+resource.post("/:resource_id/competencies", resourceController.createResourceCompetencies);
+resource.delete("/:resource_id/competencies", resourceController.deleteResourceCompetencies);
 resource.get("/:resource_id/missions", resourceController.getResourceMissions);
+resource.post("/:resource_id/missions", resourceController.createResourceMissions);
+
 
 export default resource;
